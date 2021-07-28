26ad6b078777592ab1b2914470799f774e2eef12
Brooke McGoldrick <bcmcgold@mit.edu>
2021-07-28 14:19:21 -0400

    Update oscillator.go

--- engine/oscillator.go
+++ engine/oscillator.go
@@ -7,10 +7,10 @@ var(
 	R       [4]float64              // MTJ nanopillar resistance, Ohm, function of m(t)
 	Rp      float64=float64(0.0)    // MTJ parallel resistance, Ohm, const
 	Rap     float64=float64(0.0)    // SHO Resistance in AP state, Ohm, const
-  Rout1   =NewScalarValue("Rout1","Ohm","SHNO 1 Resistance",func() float64 { return (Rap-Rp)*R[0]/2+(Rp+Rap)/2 }) //Use tableadd(Rout1) to print R, just as E_total
-  Rout2   =NewScalarValue("Rout2","Ohm","SHNO 2 Resistance",func() float64 { return (Rap-Rp)*R[1]/2+(Rp+Rap)/2 }) //Note that R[n] is -mx (negative sign)
-  Rout3   =NewScalarValue("Rout3","Ohm","SHNO 3 Resistance",func() float64 { return (Rap-Rp)*R[2]/2+(Rp+Rap)/2 })
-  Rout4   =NewScalarValue("Rout4","Ohm","SHNO 4 Resistance",func() float64 { return (Rap-Rp)*R[3]/2+(Rp+Rap)/2 })
+  Rout1   =NewScalarValue("Rout1","Ohm","SHNO 1 Resistance",func() float64 { return (Rp-Rap)*R[0]/2+(Rp+Rap)/2 }) //Use tableadd(Rout1) to print R, just as E_total
+  Rout2   =NewScalarValue("Rout2","Ohm","SHNO 2 Resistance",func() float64 { return (Rp-Rap)*R[1]/2+(Rp+Rap)/2 }) //Note that R[n] is -mx (negative sign)
+  Rout3   =NewScalarValue("Rout3","Ohm","SHNO 3 Resistance",func() float64 { return (Rp-Rap)*R[2]/2+(Rp+Rap)/2 })
+  Rout4   =NewScalarValue("Rout4","Ohm","SHNO 4 Resistance",func() float64 { return (Rp-Rap)*R[3]/2+(Rp+Rap)/2 })
   Jread   float64=float64(0.0)    // MTJ read current density (A/m2)
   Gij   [4][4]float64           // Coupling conductance matrix
   Jcpl    =NewExcitation("Jcpl", "A/m2", "Coupling electrical current density") // Coupling current density added to mumax STT term
